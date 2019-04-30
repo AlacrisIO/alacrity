9370d2743ede598b089814141e6bab7563b0f339
Francois-Rene Rideau <fare@tunes.org>
2019-04-30 17:24:35 +0000

    Improve and document the r-p-s solidity contract
    Add better documentation, better named constants.
    Fix bug in constructor: don't forget to add value.

--- examples/rock-paper-scissors.sol
+++ examples/rock-paper-scissors.sol
@@ -1,23 +1,64 @@
 pragma solidity ^0.5.2;
 
+// TODO: Should we emit an event for each transition,
+// or rely on people watching the chain?
+
+/*
+  From the DSL to the solidity contract, many optimizations were manually done,
+  that could be realistically done automatically, including:
+  - merging consecutive actions by the same participant (the initial actions by player 0):
+    - creation of contract/context
+    - sending money
+    - initializing with a message
+  - in the message exchange, specialize to the case of two people
+    - the first one is merged as above.
+    - only one is left, the second and last, that doesn't have to publish a commitment, just a hand
+    - the salt of the second is not used, therefore can be omitted (can be automated)
+  - flattening the state
+    - state is defined as plenty of global variables, sometimes undefined.
+    - continuation closures were transformed into ADTs, closure conversion happened,
+      linear analysis proved they could all be global in that contract.
+    - after the closure contents have been converted,
+      the program counter / current instruction / recipe step / machine state / etc. is represented as
+      a single number (called "state") rather than a data structure.
+ */
+
 contract RockPaperScissors
 {
         // At about 15 seconds per block, 11520 blocks is about two days for a timeout,
         // which should be safe against connectivity attacks.
         uint constant timeout_in_blocks = 11520;
 
+        // Possible hands
         uint8 constant rock = 0;
         uint8 constant paper = 1;
         uint8 constant scissors = 2;
         uint8 constant none = 255;
 
-        uint8 step = 0;
-        uint amount;
+        /** Current state of the state machine. Possible States:
+            0 - uninitialized
+            1 - player0 funded wager+escrow and published a commitment; waiting for player1
+            2 - player1 showed his hand; waiting for player0
+            255 - end of game (in the future, have a way to reset the contract to state 0?)
+         */
+        uint8 state = 0;
+        uint8 constant uninitialized = 0;
+        uint8 constant waiting_for_player1 = 1;
+        uint8 constant waiting_for_reveal = 2;
+        uint8 constant completed = 255;
+
+        /** Previous block at which state transition happened,
+            for timeouts when waiting for a player */
+        uint previous_block;
+
         address payable player0_address;
         bytes32 player0_commitment;
+        uint wager_amount;
+        uint escrow_amount;
+        bytes32 key_hash;
+
         address payable player1_address;
         uint8 hand1;
-        uint previous_block;
 
         // Utility functions
 
@@ -33,34 +74,16 @@ contract RockPaperScissors
                 require(player1_address == msg.sender);
         }
 
-        // The current invocation MUST be done by player1.
-        // If it is open to any player1, then set the player1 address.
-        function require_or_set_player1 () internal
-        {
-                if (player1_address == address(0)) {
-                        player1_address = msg.sender;
-                } else {
-                        require_player1();
-                }
-        }
-
         // Player0 wins the given amount
-        function player0_wins(uint _amount) internal
+        function player0_gets(uint _amount) internal
         {
                 player0_address.transfer(_amount);
         }
 
         // Player1 wins the given amount
-        function player1_wins(uint _amount) internal
+        function player1_gets(uint _amount) internal
         {
                 player1_address.transfer(_amount);
-        }
-
-        // Player0 and player1 both win the given amount
-        function draw(uint _amount) internal
-        {
-                player0_wins(_amount);
-                player1_wins(_amount);
         }
 
         // Timeout
@@ -68,82 +91,96 @@ contract RockPaperScissors
                 require(block.number > previous_block + timeout_in_blocks);
         }
 
-        // Step 1, called by player0 when initializing the game
+        // State 1, called by player0 when initializing the game
         // Let the address be 0 if this is an open offer, or the address of a specific opponent.
         // The commitment is a hash of some salt and the hand.
-        function fund (bytes32 _commitment, address payable _player1) external payable
+        constructor (bytes32 _commitment, bytes32 _key_hash, uint _wager_amount) public payable
         {
-                // This function can only be called while at step 0.
-                require(step == 0);
+                // This function can only be called while at state 0.
+                require(state == uninitialized);
+                require(msg.value > _wager_amount);
 
                 // Initialize the game
-                amount = msg.value;
+                wager_amount = _wager_amount;
+                escrow_amount = msg.value - wager_amount;
                 player0_address = msg.sender;
                 player0_commitment = _commitment;
-                player1_address = _player1;
+                key_hash = _key_hash;
 
-                // Set the step to 1, the block to block.number.
-                step = 1;
+                // Set the new state and previous_block
+                state = waiting_for_player1;
                 previous_block = block.number;
         }
 
-        // Step 2, called by player1 when joining the game
+        // State 2, called by player1 when joining the game
         // NB: the player1 address and amount MUST match, and the hand must be valid.
-        function play (uint8 _hand1) external payable
+        function player1_show_hand (bytes32 key, uint8 _hand1) external payable
         {
-                require(step == 1);
-                require_or_set_player1();
-                require(msg.value == amount);
+                require(state == waiting_for_player1);
+                require(keccak256(abi.encodePacked(key)) == key_hash);
+                require(msg.value == wager_amount);
                 require(_hand1 < 3);
 
+                player1_address = msg.sender;
                 hand1 = _hand1;
 
-                // Set the step to 2, the block to block.number.
-                step = 2;
+                // Set the new state and previous_block
+                state = waiting_for_reveal;
                 previous_block = block.number;
         }
 
-        // Step 3, called by player0 after player1 played, reveals the committed hand.
-        function reveal (bytes31 salt, uint8 hand0) external payable {
-                require(step == 2);
+        // State 3, called by player0 after player1 played, reveals the committed hand.
+        function player0_reveal (bytes31 salt, uint8 hand0) external payable {
+                require(state == waiting_for_reveal);
                 require_player0();
-                uint8 diff = (hand0 - hand1) % 3;
                 if (hand0 >= 3 || player0_commitment != keccak256(abi.encodePacked(salt, hand0))) {
-                        player1_wins(2*amount);
-                } else if (diff == 1) {
-                        player0_wins(2*amount);
-                } else if (diff == 2) {
-                        player1_wins(2*amount);
+                        // If the reveal is invalid, player1 wins and player0 loses escrow.
+                        player1_gets(2*wager_amount+escrow_amount);
                 } else {
-                        draw(amount);
+                        // compute difference modulo 3 without underflowing
+                        uint8 diff = (hand0 + 3 - hand1) % 3;
+
+                        if (diff == 1) {
+                                // The reveal is in favor of player0
+                                player0_gets(2*wager_amount+escrow_amount);
+                        } else if (diff == 2) {
+                                // The reveal is in favor of player1
+                                player1_gets(2*wager_amount);
+                                player0_gets(escrow_amount);
+                        } else {
+                                // The reveal is a draw
+                                player1_gets(wager_amount);
+                                player0_gets(wager_amount+escrow_amount);
+                        }
                 }
-                step = 0 ;
+                state = completed;
         }
 
-        // Step 2 bis, rescind the offer to play -- called by player0 after player1 times out.
-        function rescind () external payable {
-                require(step == 1);
+        // State 2 bis, rescind the offer to play -- called by player0 after player1 times out.
+        function player0_rescind () external payable {
+                require(state == waiting_for_player1);
                 require_player0();
                 require_timeout();
-                player0_wins(amount);
-                step = 0;
+                player0_gets(wager_amount+escrow_amount);
+                state = completed;
         }
 
-        // Step 3 bis, win by default -- called by player1 after player0 times out rather than reveal hand.
-        function win_by_default () external payable {
-                require(step == 2);
+        // State 3 bis, win by default -- called by player1 after player0 times out rather than reveal hand.
+        function player1_win_by_default () external payable {
+                require(state == waiting_for_reveal);
                 require_player1();
                 require_timeout();
-                player1_wins(amount);
-                step = 0;
+                player1_gets(wager_amount+escrow_amount);
+                state = completed;
         }
 }
 
 contract RockPaperScissorsFactory
 {
-        function createRockPaperScissors (bytes32 _commitment, address payable _player1) public payable
+        function createRockPaperScissors
+                (bytes32 _commitment, bytes32 _key_hash, uint _wager_amount) public payable
         {
-                RockPaperScissors c = new RockPaperScissors();
-                c.fund(_commitment, _player1);
+                (new RockPaperScissors).value(msg.value)(_commitment, _key_hash, _wager_amount);
+                // NB: Not returning the address; you have to get it from the transaction receipt.
         }
 }
