1496289ef0045b0a100a75fb9826b8ea6215bbce
Jay McCarthy <jay.mccarthy@gmail.com>
2019-06-14 15:23:35 -0400

    Fixing JS emit

---
+++ js/stdlib.mjs
@@ -0,0 +1,7 @@
+export function assert(cond) { XXX; }
+export function int2bytes(i) { XXX; }
+export function keccak256(b) { XXX; }
+export function bytes_cat(b) { XXX; }
+export function bytes_left(b) { XXX; }
+export function bytes_right(b) { XXX; }
+export function random() { XXX; }
