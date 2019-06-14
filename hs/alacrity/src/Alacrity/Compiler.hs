dbe857472d2b04a53f024504977e59cd569321d6
Jay McCarthy <jay.mccarthy@gmail.com>
2019-06-14 15:30:37 -0400

    Reorganize

--- hs/alacrity/src/Alacrity/Compiler.hs
+++ hs/alacrity/src/Alacrity/Compiler.hs
@@ -1,21 +1,19 @@
-{-# LANGUAGE TemplateHaskell #-}
 module Alacrity.Compiler where
 --import Debug.Trace
 
 import Control.Monad.State.Lazy
 import qualified Data.Map.Strict as M
 import qualified Data.Set as Set
-import Data.FileEmbed
 import Data.Foldable
 import qualified Data.Sequence as S
 import Data.Text.Prettyprint.Doc
 import System.Exit
-import Z3.Monad as Z3
 
 import Alacrity.AST
 import Alacrity.Parser
-import Alacrity.EmitJS2
+import Alacrity.EmitJS
 import Alacrity.EmitSol
+import Alacrity.EmitZ3
 
 {- Inliner
 
@@ -520,131 +518,6 @@ epp (IL_Prog ips it) = BL_Prog bps cp
         initarg ((n, s), et) = ((n, s), (et, Secret))
         γ = M.insert RoleContract M.empty γi
 
-
-{- Z3 Theory Generation
-
-   The Z3 theory has to prove a few different things.
-
-   1. The balance of CTC at the end of the protocol is 0. It will have
-   to do this by employing something like the State monad to represent
-   all the various modifications to the CTC value overtime and assert
-   that it is 0 at the end. This ensure that the protocol doesn't
-   "leave anything on the table".
-
-   2. For each assert! in the program, assuming that all previous
-   assert!s are true (including those in other participants and the
-   contract) and DISHONEST is FALSE, the assert is true.
-
-   3. For each assert! in the program, assuming that all previous
-   assert!s are true (EXCLUDING those in other participants, but
-   INCLUDING the contract) and DISHONEST is TRUE, the assert is true.
-
-   When
-
-   #1 has to be done in both #2 and #3 modes, because the transfer
-   amounts may rely on participant data.
-
- -}
-
-primZ3Runtime :: Z3.Z3 Z3.AST
-primZ3Runtime =
-  parseSMTLib2String $(embedStringFile "../../z3/z3-runtime.smt2") [] [] [] []
-
-exprTypeZ3 :: ExprType -> Z3.Z3 Z3.Sort
-exprTypeZ3 (TY_Con AT_Unit) = error "z3 sort `Unit`"
-exprTypeZ3 (TY_Con AT_Int) = Z3.mkIntSort
-exprTypeZ3 (TY_Con AT_Bool) = Z3.mkBoolSort
-exprTypeZ3 (TY_Con AT_Bytes) = error "z3 sort `Bytes`"
-exprTypeZ3 (TY_Var _) = error "type variables not yet supported"
-
-cPrimZ3NoargOp :: (Z3.Z3 Z3.AST) -> ([Z3.AST] -> Z3.Z3 Z3.AST)
-cPrimZ3NoargOp op [] = op
-cPrimZ3NoargOp _ lst =
-  error ("no-arg operation: expected 0 arguments, received " ++ show (length lst))
-
-cPrimZ3BinOp :: (Z3.AST -> Z3.AST -> Z3.Z3 Z3.AST) -> ([Z3.AST] -> Z3.Z3 Z3.AST)
-cPrimZ3BinOp op [a, b] = op a b
-cPrimZ3BinOp _ lst =
-  error ("binary operation: expected 2 arguments, received " ++ show (length lst))
-
-cPrimZ3TernOp :: (Z3.AST -> Z3.AST -> Z3.AST -> Z3.Z3 Z3.AST) -> ([Z3.AST] -> Z3.Z3 Z3.AST)
-cPrimZ3TernOp op [a, b, c] = op a b c
-cPrimZ3TernOp _ lst =
-  error ("ternary operation: expected 3 arguments, received " ++ show (length lst))
-
-cPrimZ3Fun :: String -> FunctionType -> ([Z3.AST] -> Z3.Z3 Z3.AST)
-cPrimZ3Fun fstr (TY_Arrow intys outty) ins =
-  do fsym <- Z3.mkStringSymbol fstr
-     insorts <- mapM exprTypeZ3 intys
-     outsort <- exprTypeZ3 outty
-     f <- Z3.mkFuncDecl fsym insorts outsort
-     Z3.mkApp f ins
-cPrimZ3Fun _ (TY_Forall _ _) _ =
-  error "forall not yet supported"
-
-cPrimZ3 :: C_Prim -> Bool -> [Z3.AST] -> Z3.Z3 Z3.AST
--- cPrimZ3 op dishon ins = out
--- relies on the declarations from `primZ3Runtime` being available
-cPrimZ3 ADD _ = mkAdd
-cPrimZ3 SUB _ = mkSub
-cPrimZ3 MUL _ = mkMul
-cPrimZ3 DIV _ = cPrimZ3BinOp mkDiv
-cPrimZ3 MOD _ = cPrimZ3BinOp mkMod
-cPrimZ3 PLT _ = cPrimZ3BinOp mkLt
-cPrimZ3 PLE _ = cPrimZ3BinOp mkLe
-cPrimZ3 PEQ _ = cPrimZ3BinOp mkEq
-cPrimZ3 PGE _ = cPrimZ3BinOp mkGe
-cPrimZ3 PGT _ = cPrimZ3BinOp mkGt
-cPrimZ3 IF_THEN_ELSE _ = cPrimZ3TernOp mkIte
-cPrimZ3 INT_TO_BYTES _ = cPrimZ3Fun "integer->integer-bytes" (primType (CP INT_TO_BYTES))
-cPrimZ3 DIGEST _ = cPrimZ3Fun "digest" (primType (CP DIGEST))
-cPrimZ3 BYTES_EQ _ = cPrimZ3BinOp mkEq
-cPrimZ3 BYTES_LEN _ = cPrimZ3Fun "bytes-length" (primType (CP BYTES_LEN))
-cPrimZ3 BCAT _ = cPrimZ3Fun "msg-cat" (primType (CP BCAT))
-cPrimZ3 BCAT_LEFT _ = cPrimZ3Fun "msg-left" (primType (CP BCAT_LEFT))
-cPrimZ3 BCAT_RIGHT _ = cPrimZ3Fun "msg-right" (primType (CP BCAT_RIGHT))
-cPrimZ3 DISHONEST dishon = cPrimZ3NoargOp (mkBool dishon)
-
-primZ3 :: EP_Prim -> Bool -> [Z3.AST] -> Z3.AST -> Z3.Z3 ()
--- primZ3 op dishon ins out = assertion
--- relies on the declarations from `primZ3Runtime` being available
-primZ3 (CP op) dishon ins out =
-  do op_call <- cPrimZ3 op dishon ins
-     eq_call <- Z3.mkEq op_call out
-     Z3.assert eq_call
-primZ3 RANDOM _ [] _ =
-  -- the arguments don't specify any constraint
-  return ()
-primZ3 RANDOM _ [x] o =
-  -- the arguments specify 0 <= o < x
-  do z <- Z3.mkInteger 0
-     zleo <- Z3.mkLe z o
-     oltx <- Z3.mkLt o x
-     zleoltx <- Z3.mkAnd [zleo, oltx]
-     Z3.assert zleoltx
-primZ3 INTERACT _ [_] _ =
-  -- no constraint
-  return ()
-primZ3 _ _ _ _ = error "XXX fill in Z3 primitives"
-
-emit_z3 :: BLProgram -> Z3.Z3 [String]
-emit_z3 _
- = do
-  Z3.push
-  f <- Z3.mkFalse
-  Z3.assert f
-  (res, mm) <- Z3.solverCheckAndGetModel
-  Z3.pop 1
-  case res of
-    Z3.Unsat -> return []
-    Z3.Sat ->
-      case mm of
-        Nothing -> return ["Problem; no model!"]
-        Just m -> do
-          s <- modelToString m
-          return [s]
-    Z3.Undef -> return []
-
 compile :: FilePath -> IO ()
 compile srcp = do
   xlp <- readAlacrityFile srcp
@@ -655,14 +528,8 @@ compile srcp = do
   writeFile (srcp ++ ".il") (show (pretty ilp))
   let blp = epp ilp
   writeFile (srcp ++ ".bl") (show (pretty blp))
-  z3res <- evalZ3 (emit_z3 blp)
-  case z3res of
-    [] -> do
-      writeFile (srcp ++ ".sol") (show (emit_sol blp))
-      writeFile (srcp ++ ".js") (show (emit_js blp))
-      exitSuccess
-    ps -> do
-      mapM_ (\x -> putStrLn $ ("Z3 error:" ++ x)) ps
-      die "Z3 failed to verify!"
-
-
+  check_z3 (emit_z3 blp)
+  writeFile (srcp ++ ".sol") (show (emit_sol blp))
+  writeFile (srcp ++ ".js") (show (emit_js blp))
+  exitSuccess
+    
--- hs/alacrity/src/Alacrity/EmitJS.hs
+++ hs/alacrity/src/Alacrity/EmitJS.hs
@@ -1,13 +1,8 @@
 module Alacrity.EmitJS where
 
---import Control.Monad.State.Lazy
---import qualified Data.Map.Strict as M
---import Data.Foldable
---import qualified Data.Sequence as S
-import Language.JavaScript.Parser as JS
-import Language.JavaScript.Parser.Parser as JSParser
-import Language.JavaScript.Parser.AST as JSAST
-import System.IO
+import qualified Data.Map.Strict as M
+import Data.List (intersperse)
+import Data.Text.Prettyprint.Doc
 
 import Alacrity.AST
 
@@ -26,83 +21,137 @@ import Alacrity.AST
    with the result.
   -}
 
-as_js :: BLProgram -> JS.JSAST
-as_js (BL_Prog _blparts (C_Prog _ _handlers)) = JS.JSAstModule
-  (module_header ++ []) JSAnnotSpace
+jsVar :: BLVar -> Doc a
+jsVar (n, _, _) = pretty $ "v" ++ show n
 
-emit_js :: BLProgram -> String
-emit_js blp = JS.renderToString $ as_js blp
+jsVarType :: BLVar -> Doc a
+jsVarType (_, _, bt) = pretty $ "\"" ++ solType bt ++ "\""
 
-toJsl :: [a] -> JSAST.JSCommaList a
-toJsl = toJsl' . reverse
+jsCon :: Constant -> Doc a
+jsCon (Con_I i) = pretty i
+jsCon (Con_B True) = pretty "true"
+jsCon (Con_B False) = pretty "false"
+jsCon (Con_BS s) = pretty $ "\"" ++ show s ++ "\""
 
-toJsl' :: [a] -> JSAST.JSCommaList a
-toJsl' [] = JSAST.JSLNil
-toJsl' [a] = JSAST.JSLOne a
-toJsl' (a:as) = JSAST.JSLCons (toJsl' as) JSAST.JSNoAnnot a
+jsArg :: BLArg -> Doc a
+jsArg (BL_Var v) = jsVar v
+jsArg (BL_Con c) = jsCon c
 
-js_import_names :: [String] -> JSAST.JSImportClause
-js_import_names =
-  (\x -> JSAST.JSImportClauseNamed (JSAST.JSImportsNamed JS.JSNoAnnot x JS.JSNoAnnot)) .
-  toJsl .
-  map (\name -> JSAST.JSImportSpecifier (JSAST.JSIdentName JSAST.JSNoAnnot name))
+jsPartVar :: Participant -> Doc a
+jsPartVar p = pretty $ "p" ++ p
 
-js_imports :: String -> [String] -> JSAST.JSModuleItem
-js_imports module_name identifiers =
-  JSAST.JSModuleImportDeclaration JS.JSNoAnnot
-    (JSAST.JSImportDeclaration
-     (js_import_names identifiers)
-     (JSAST.JSFromClause JS.JSAnnotSpace JS.JSAnnotSpace module_name)
-     (JS.JSSemi JS.JSNoAnnot))
+jsVarDecl :: BLVar -> Doc a
+jsVarDecl bv = pretty "var" <+> jsVar bv
 
-local_module_name :: String -> String
-local_module_name s = "\"./" ++ s ++ ".mjs\""
+jsBraces :: Doc a -> Doc a
+jsBraces body = braces (nest 2 $ hardline <> body <> space)
 
-js_imports_local :: String -> [String] -> JSAST.JSModuleItem
-js_imports_local = js_imports . local_module_name
+jsArray :: [Doc a] -> Doc a
+jsArray elems = brackets $ hcat $ intersperse (comma <> space) elems
 
-module_header :: [JSAST.JSModuleItem]
-module_header =
-  [js_imports_local "dsl-api"
-    ["byteToHex", "registerInit", "hexToAddress", "hexTo0x", "checkRequirement",
-     "loggedAlert", "merge", "flip", "logErrorK", "randomSalt", "logging", "kLogResult", "kLogError",
-     "web3", "crypto", "userAddress",
-     "saltedDigest", "registerBackendHooks", "renderGame", "config",
-     "toBN", "optionalAddressOf0x", "optionalAddressMatches", "hexToBigNumber", "deployContract",
-     "getGame", "updateGame", "removeActiveGame", "queueGame", "attemptGameCreation",
-     "optionalAddressTo0x", "isGameConfirmed", "digestHex", "sendTx",
-     "renderWei",
-     "contract", "contractAt", "contractFactory"]]
+jsApply :: String -> [Doc a] -> Doc a
+jsApply f args = pretty f <> parens (hcat $ intersperse (comma <> space) args)
 
---jsconst :: String -> -> JSAST.JSModuleItem
---jsconst :: JSAST.JSModuleItem
---    JSModuleExportDeclaration JSNoAnnot (JSExport (JSConst JSNoAnnot (JSLOne (JSVarInitExpression (JSIdentifier JSNoAnnot "rpsFactory") JSVarInitNone)) (JSSemi JSNoAnnot)) JSSemiAuto),
+jsFunction :: String -> [Doc a] -> Doc a -> Doc a
+jsFunction name args body =
+  pretty "function" <+> jsApply name args <+> jsBraces body
 
+jsLambda :: [Doc a] -> Doc a -> Doc a
+jsLambda args body = jsApply "" args <+> pretty "=>" <+> jsBraces body
 
+jsReturn :: Doc a -> Doc a
+jsReturn a = pretty "return" <+> a <> semi
 
--- Below stuff for debug purposes (Fare)
--- | Parse JavaScript Module (Script)
--- Parse one compound statement, or a sequence of simple statements.
--- Generally used for interactive input, such as from the command line of an interpreter.
--- Return comments in addition to the parsed statements.
-parseJS :: String -- ^ The name of the Javascript source (filename or input device).
-     -> IO (Either String JS.JSAST)
-        -- ^ An error or maybe the abstract syntax tree (AST) of zero
-        -- or more Javascript statements, plus comments.
-parseJS filename = do
-  x <- System.IO.readFile filename
-  return (JSParser.parseModule x filename)
+jsObject :: [(String, Doc a)] -> Doc a
+jsObject kvs = jsBraces $ vsep $ (intersperse comma) $ map jsObjField kvs
+  where jsObjField (k, v) = pretty (k ++ ":") <> hardline <> v
 
-dbj :: String
-dbj = "../../examples/rps-demo/dapp-backend.mjs.lj"
+jsBinOp :: String -> Doc a -> Doc a -> Doc a
+jsBinOp o l r = l <+> pretty o <+> r
 
-pjb :: a -> IO ()
-pjb _ = do
-  x <- parseJS dbj
-  writeFile (dbj ++ ".jsast") (show x)
+jsPrimApply :: EP_Prim -> [Doc a] -> Doc a
+jsPrimApply pr args =
+  case pr of
+    CP ADD -> binOp "+"
+    CP SUB -> binOp "-"
+    CP MUL -> binOp "*"
+    CP DIV -> binOp "/"
+    CP MOD -> binOp "%"
+    CP PLT -> binOp "<"
+    CP PLE -> binOp "<="
+    CP PEQ -> binOp "=="
+    CP PGE -> binOp ">="
+    CP PGT -> binOp ">"
+    CP IF_THEN_ELSE -> case args of
+                      [ c, t, f ] -> c <+> pretty "?" <+> t <+> pretty ":" <+> f
+                      _ -> spa_error ()
+    CP INT_TO_BYTES -> jsApply "stdlib.int2bytes" args
+    CP DIGEST -> case args of
+                [ a ] -> jsApply "stdlib.keccak256" [a]
+                _ -> spa_error ()
+    CP BYTES_EQ -> binOp "=="
+    CP BYTES_LEN -> case args of
+                   [ a ] -> a <> pretty ".length"
+                   _ -> spa_error ()
+    CP BCAT -> jsApply "stdlib.bytes_cat" args
+    CP BCAT_LEFT -> jsApply "stdlib.bytes_left" args
+    CP BCAT_RIGHT -> jsApply "stdlib.bytes_right" args
+    CP DISHONEST -> case args of
+                   [] -> jsCon (Con_B True)
+                   _ -> spa_error ()
+    RANDOM -> jsApply "stdlib.random" args
+    INTERACT -> error "interact doesn't use jsPrimApply"
+  where binOp op = case args of
+          [ l, r ] -> jsBinOp op l r
+          _ -> spa_error ()
+        spa_error () = error "jsPrimApply"
 
-pjf :: a -> IO (Either String JS.JSAST)
-pjf _ = parseJS "/tmp/foo.mjs"
+jsEPExpr :: [Participant] -> EPExpr -> Doc a
+jsEPExpr _ (EP_Arg a) = jsArg a
+jsEPExpr _ (EP_PrimApp pr al) = jsPrimApply pr $ map jsArg al
+jsEPExpr _ (EP_Assert a) = jsApply "stdlib.assert" [ jsArg a ]
+jsEPExpr _ (EP_Send i svs msg amt) = jsApply "net.send" [ pretty i, ts, vs, jsArg amt ]
+  where args = svs ++ msg
+        ts = jsArray $ map jsVarType args
+        vs = jsArray $ map jsVar args
 
-pjs :: String -> Either String JS.JSAST
-pjs x = JSParser.parseModule x "stdin"
+jsEPTail :: [Participant] -> EPTail -> Doc a
+jsEPTail _ (EP_Ret al) = jsApply "kTop" $ map jsArg al
+jsEPTail ps (EP_If ca tt ft) =
+  pretty "if" <+> parens (jsArg ca) <> bp tt <> hardline <> pretty "else" <> bp ft
+  where bp at = jsBraces $ jsEPTail ps at
+jsEPTail ps (EP_Let mv (EP_PrimApp INTERACT al) kt) =
+  jsApply "interact" ((map jsArg al) ++ [ kp ])
+  where kp = jsLambda kargs $ jsEPTail ps kt
+        kargs = case mv of
+                  Nothing -> []
+                  Just bv -> [ jsVar bv ]
+jsEPTail ps (EP_Let Nothing ee kt) = vsep [ jsEPExpr ps ee <> semi, jsEPTail ps kt ];
+jsEPTail ps (EP_Let (Just bv) ee kt) = vsep [ jsVarDecl bv <+> pretty "=" <+> jsEPExpr ps ee <> semi, jsEPTail ps kt ];
+jsEPTail ps (EP_Recv i _ msg kt) = jsApply "net.recv" [ pretty i, msg_ts, kp ]
+  where kp = jsLambda msg_vs (jsEPTail ps kt)
+        msg_ts = jsArray $ map jsVarType msg
+        msg_vs = map jsVar msg
+
+jsPart :: [Participant] -> Participant -> (Participant, EProgram) -> (String, Doc a)
+jsPart ps initiator (p, (EP_Prog pargs et)) = (p, partp)
+  where ps_vs = map jsPartVar ps
+        pargs_vs = map jsVar pargs
+        ctc_v = pretty "ctc"
+        part_args = if initiator == p then ps_vs
+                    else ctc_v : ps_vs        
+        netcall = if initiator == p then "net.make" else "net.attach"
+        ncargs = part_args ++ [ kp ]
+        all_args = part_args ++ pargs_vs ++ [pretty "kTop"]
+        first_call = jsApply netcall ncargs <> semi
+        partp = jsLambda all_args first_call
+        kp = jsLambda [] (jsEPTail ps et)
+
+emit_js :: BLProgram -> Doc a
+emit_js (BL_Prog _ (C_Prog _ [])) =
+  error "emit_js: Cannot create contract with no consensus"
+emit_js (BL_Prog pm (C_Prog ps (C_Handler initiator _ _ _ : _))) = modp
+  where modp = vsep [ pretty "import * as stdlib from 'stdlib.mjs';", emptyDoc,
+                      pretty "export" <+> jsFunction "initialize" [ pretty "net", pretty "interact" ] bodyp ]
+        bodyp = jsReturn objp
+        objp = jsObject $ map (jsPart ps initiator) $ M.toList pm
