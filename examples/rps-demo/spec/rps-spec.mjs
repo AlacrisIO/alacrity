76e16f513646c2fc63120be1a6146e2595fd67e7
Matt Audesse <matt@mattaudesse.com>
2019-06-17 12:01:35 -0400

    rps-spec.mjs WIP based on discussion w/ Chris

--- examples/rps-demo/spec/rps-spec.mjs
+++ examples/rps-demo/spec/rps-spec.mjs
@@ -1,13 +1,15 @@
 // vim: filetype=javascript
 
 import '../monkey-patch-require.js';
-import '../rps-demo.mjs'; // Trigger effectful module registration
-import { web3          } from '../web3-prelude.mjs';
-import { ethToWei      } from '../common-runtime.mjs';
-import { createNewGame } from '../dapp-backend.mjs';
+import * as A   from '../dsl-api.mjs';
+import { web3 } from '../web3-prelude.mjs';
 
 const panic = m =>
   console.error(m) || process.exit(1);
+
+const balanceOf = a =>
+  web3.eth.getBalance(a);
+
 
 // This matches the logic in legicash-facts'
 // src/legilogic_ethereum/ethereum_transaction.ml:get_first_account function
@@ -15,17 +17,28 @@ const panic = m =>
 const PREFUNDED_PRIVATE_NET_ACCT =
   web3.personal.listAccounts[0] || panic('Cannot infer prefunded account!');
 
-// See config/demo-keys-small.json in legicash-facts
-const ALICE = '0xc54e86dffb87b9736e2e35dd85c775358f1c31ce';
-const BOB   = '0x9ccaed210ce8c0cb49c5ad1c4f583406c264ba69';
+const accts = {};
 
 
-const prefundAliceAndBob = () => {
+// Upserts (nickname, hex ID) record
+const createAndUnlock = acctNames => {
+  const created = u =>
+    new Promise((resolve, reject) =>
+      web3.personal.newAccount((z, i) => {
+        accts[u] = i;
+        return web3.personal.unlockAccount(i, resolve);
+      }));
+
+  return Promise.all(acctNames.map(created));
+};
+
+
+const prefundTestAccounts = () => {
   // https://github.com/ethereum/wiki/wiki/JavaScript-API#web3ethsendtransaction
   const newTxFor = to =>
     ({ to
      , from:  PREFUNDED_PRIVATE_NET_ACCT
-     , value: ethToWei(100)
+     , value: A.ethToWei(100)
      });
 
   const prefunded = acct =>
@@ -34,29 +47,72 @@ const prefundAliceAndBob = () => {
         !!e ? reject(e)
             : resolve()));
 
-  return Promise.all([ prefunded(ALICE), prefunded(BOB) ]);
+  return Promise.all(Object.values(accts).map(prefunded));
 };
 
-describe('A rock/paper/scissors game', () => {
 
-  beforeAll(done => {
-    // Web3's internals will break without this:
-    web3.eth.defaultAccount = PREFUNDED_PRIVATE_NET_ACCT;
+const runPrep = done => {
+  // Web3's internals will break without this:
+  web3.eth.defaultAccount = PREFUNDED_PRIVATE_NET_ACCT;
 
-    prefundAliceAndBob()
-      .then(done)
-      .catch(panic)
+  return createAndUnlock([ 'alice', 'bob' ])
+    .then(prefundTestAccounts)
+    .then(done)
+    .catch(panic)
+};
+
+
+describe('The test suite can easily', () => {
+  beforeAll(runPrep);
+
+  describe('create + prefund new test accounts', () => {
+
+    it('which can exchange funds', done => {
+      const balanceStartAlice = balanceOf(accts.alice);
+      const balanceStartBob   = balanceOf(accts.bob);
+
+      const value = 50000;
+      const tx    = { from: accts.alice, to: accts.bob, value };
+
+      web3.eth.sendTransaction(tx, () => {
+        const balanceEndAlice = balanceOf(accts.alice);
+        const balanceEndBob   = balanceOf(accts.bob);
+
+        expect(balanceEndAlice).toBeLessThan(balanceStartAlice - value);
+        expect(balanceEndBob).toBeGreaterThan(balanceStartBob + value);
+
+        done();
+      });
+    });
   });
+});
+
+
+xdescribe('A rock/paper/scissors game', () => {
+  beforeAll(runPrep);
 
   describe('results in', () => {
 
     it('both participants agreeing on who won', () => {
     });
 
-    it('the winner\'s balance being increased + loser\'s balance being reduced by wager', () => {
-      const wagerInWei  = ethToWei(1.5);
-      const escrowInWei = wagerInWei.div(10);
-      createNewGame(wagerInWei, escrowInWei, BOB, 1);
+    it('the winner\'s balance being increased + loser\'s balance being reduced by wager', done => {
+      // TODO
+      // Switch to Alice account before creating new game
+      // Infer game ID
+      // Switch to Bob account and accept
+      // Await confirmation of game completion on chain
+      // Look up Alice's new balance
+      // Look up Bob's new balance
+      // Compare and assert balances
+      // Salt generation must be fixed for `node`
+      const balanceStartAlice = balanceOf(accts.alice);
+      const balanceStartBob   = balanceOf(accts.bob);
+      const wagerInWei        = A.ethToWei(1.5);
+      const escrowInWei       = wagerInWei.div(10);
+
+      done();
+
     });
   });
 });
