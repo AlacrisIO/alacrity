c6487607d8822045e03c74090c2fcc42595741d1
Francois-Rene Rideau <fare@tunes.org>
2019-06-20 04:00:27 -0400

    Do update ("overwrite") solc outputs during tests

--- hs/alacrity/test/Spec.hs
+++ hs/alacrity/test/Spec.hs
@@ -6,6 +6,6 @@ import Alacrity.Util
 main :: IO ()
 main = do
   maybeDie $ system "stack exec alacrityc ala/rps.ala"
-  maybeDie $ system "REPO=$(dirname \"$(dirname \"$PWD\")\") ; solc --allow-paths $REPO -o ala/_build --bin --abi ala/rps.ala.sol"
+  maybeDie $ system "REPO=$(dirname \"$(dirname \"$PWD\")\") ; solc --allow-paths $REPO -o ala/_build --bin --abi --overwrite ala/rps.ala.sol"
   --- XXX maybeDie $ system "z3 ala/rps.ala.z3"
   exitSuccess
