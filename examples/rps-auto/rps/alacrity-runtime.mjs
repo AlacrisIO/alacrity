4fae6558a48ae0d4104674904cfc866e1a5c702d
Matt Audesse <matt@mattaudesse.com>
2019-06-22 20:15:54 -0400

    Fix tab indentation in rps-auto/rps/alacrity-runtime.mjs

--- examples/rps-auto/rps/alacrity-runtime.mjs
+++ examples/rps-auto/rps/alacrity-runtime.mjs
@@ -21,18 +21,18 @@ const isBigInt = x =>
 // Used by msg_cat to encode the left_length 16-bit unsigned integer
 // as 2 hex bytes or 4 hex characters
 const nat16_to_fixed_size_hex = n => {
-	// 2^16 = 1 << 16
-	if (!(Number.isInteger(n) && 0 <= n && n < (1 << 16))) {
-		console.error('nat16_to_fixed_size_hex: expected a nat that can fit into 16 bits');
-	}
-	// 16 bits = 2 bytes = 4 hex characters
-	return n.toString(16).padStart(4, '0');
+  // 2^16 = 1 << 16
+  if (!(Number.isInteger(n) && 0 <= n && n < (1 << 16))) {
+    console.error('nat16_to_fixed_size_hex: expected a nat that can fit into 16 bits');
+  }
+  // 16 bits = 2 bytes = 4 hex characters
+  return n.toString(16).padStart(4, '0');
 };
 
 // Used by both msg_left and msg_right to see when the left stops and the right
 // starts - 16 bits = 2 bytes = 4 hex characters
 const bytes_left_length = m =>
-	parseInt(m.substring(0, 4), 16);
+  parseInt(m.substring(0, 4), 16);
 
 // In our JS representation of byte strings, a byte
 // is a pair of hex chars, so the "JS length" is twice
@@ -78,11 +78,11 @@ const bytes_eq = web3 => (x, y) =>
 
 // TODO `Number.isInteger` probably isn't sufficient on its own here
 const bytes_len = web3 => b => {
-	const bh = hexOf(web3)(b);
+  const bh = hexOf(web3)(b);
   const n  = bh.length / 2;
 
-	return !Number.isInteger(n)
-		? panic(`Invalid byte string: ${bh}`)
+  return !Number.isInteger(n)
+    ? panic(`Invalid byte string: ${bh}`)
     : n;
 };
 
@@ -97,7 +97,7 @@ const bytes_cat = web3 => (a, b) => {
 };
 
 const random_uint256 = (web3, random32Bytes) => () =>
-	hexToBN(web3)(byteArrayToHex(random32Bytes()));
+  hexToBN(web3)(byteArrayToHex(random32Bytes()));
 
 
 // TODO these should probably just assume BigNumber arguments
