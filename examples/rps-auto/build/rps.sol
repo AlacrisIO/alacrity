c8924e9e2dcf5f164f7463339b6c57eff57c91f1
Jay McCarthy <jay.mccarthy@gmail.com>
2019-06-29 16:53:14 -0400

    simplify rps

--- examples/rps-auto/build/rps.sol
+++ examples/rps-auto/build/rps.sol
@@ -52,40 +52,40 @@ contract ALAContract is Stdlib {
   constructor(address payable pA, address payable pB) public payable {
     current_state = uint256(keccak256(abi.encodePacked(uint256(0), pA, pB))); }
   
-  event e0(uint256 v17, uint256 v18, uint256 v16);
-  function m0(address payable pA, address payable pB, uint256 v17, uint256 v18, uint256 v16) external payable {
+  event e0(uint256 v15, uint256 v16, uint256 v14);
+  function m0(address payable pA, address payable pB, uint256 v15, uint256 v16, uint256 v14) external payable {
     require(current_state == uint256(keccak256(abi.encodePacked(uint256(0), pA, pB))));
     require(msg.sender == pA);
-    require((msg.value == (v17 + v18)));
-    emit e0(v17, v18, v16);
-    current_state = uint256(keccak256(abi.encodePacked(uint256(1), pA, pB, v16, v17, v18))); }
+    require((msg.value == (v15 + v16)));
+    emit e0(v15, v16, v14);
+    current_state = uint256(keccak256(abi.encodePacked(uint256(1), pA, pB, v14, v15, v16))); }
   
-  event e1(uint256 v23);
-  function m1(address payable pA, address payable pB, uint256 v16, uint256 v17, uint256 v18, uint256 v23) external payable {
-    require(current_state == uint256(keccak256(abi.encodePacked(uint256(1), pA, pB, v16, v17, v18))));
+  event e1(uint256 v21);
+  function m1(address payable pA, address payable pB, uint256 v14, uint256 v15, uint256 v16, uint256 v21) external payable {
+    require(current_state == uint256(keccak256(abi.encodePacked(uint256(1), pA, pB, v14, v15, v16))));
     require(msg.sender == pB);
-    require((msg.value == v17));
-    require(((uint256(0) <= v23) ? (v23 < uint256(3)) : false));
-    emit e1(v23);
-    current_state = uint256(keccak256(abi.encodePacked(uint256(2), pA, pB, v16, v17, v18, v23))); }
+    require((msg.value == v15));
+    require(((uint256(0) <= v21) ? (v21 < uint256(3)) : false));
+    emit e1(v21);
+    current_state = uint256(keccak256(abi.encodePacked(uint256(2), pA, pB, v14, v15, v16, v21))); }
   
-  event e2(uint256 v29, uint256 v30);
-  function m2(address payable pA, address payable pB, uint256 v16, uint256 v17, uint256 v18, uint256 v23, uint256 v29, uint256 v30) external payable {
-    require(current_state == uint256(keccak256(abi.encodePacked(uint256(2), pA, pB, v16, v17, v18, v23))));
+  event e2(uint256 v27, uint256 v28);
+  function m2(address payable pA, address payable pB, uint256 v14, uint256 v15, uint256 v16, uint256 v21, uint256 v27, uint256 v28) external payable {
+    require(current_state == uint256(keccak256(abi.encodePacked(uint256(2), pA, pB, v14, v15, v16, v21))));
     require(msg.sender == pA);
     require((msg.value == uint256(0)));
-    require((v16 == (uint256(keccak256(abi.encodePacked((ALA_BCAT((abi.encodePacked(v29)), (abi.encodePacked(v30))))))))));
-    require(((uint256(0) <= v30) ? (v30 < uint256(3)) : false));
-    bool v43 = (uint256(0) <= v30) ? (v30 < uint256(3)) : false;
-    bool v46 = (uint256(0) <= v23) ? (v23 < uint256(3)) : false;
-    uint256 v53 = (v43 ? v46 : false) ? ((v30 + (uint256(4) - v23)) % uint256(3)) : (v43 ? uint256(2) : (v46 ? uint256(0) : uint256(1)));
+    require((v14 == (uint256(keccak256(abi.encodePacked((ALA_BCAT((abi.encodePacked(v27)), (abi.encodePacked(v28))))))))));
+    require(((uint256(0) <= v28) ? (v28 < uint256(3)) : false));
+    bool v41 = (uint256(0) <= v28) ? (v28 < uint256(3)) : false;
+    bool v44 = (uint256(0) <= v21) ? (v21 < uint256(3)) : false;
+    uint256 v51 = (v41 ? v44 : false) ? ((v28 + (uint256(4) - v21)) % uint256(3)) : (v41 ? uint256(2) : (v44 ? uint256(0) : uint256(1)));
     
     
     
-    bool v69 = v53 == uint256(2);
-    bool v71 = v53 == uint256(0);
-    pA.transfer((v18 + (v69 ? (uint256(2) * v17) : (v71 ? uint256(0) : v17))));
-    pB.transfer((v69 ? uint256(0) : (v71 ? (uint256(2) * v17) : v17)));
-    emit e2(v29, v30);
+    bool v67 = v51 == uint256(2);
+    bool v69 = v51 == uint256(0);
+    pA.transfer((v16 + (v67 ? (uint256(2) * v15) : (v69 ? uint256(0) : v15))));
+    pB.transfer((v67 ? uint256(0) : (v69 ? (uint256(2) * v15) : v15)));
+    emit e2(v27, v28);
     current_state = 0x0;
     selfdestruct(address(0x02B463784Bc1a49f1647B47a19452aC420DFC65A)); } }
