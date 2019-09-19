391672daf0c4ac1d355d4cd8f4c3fbd61bc8fb39
Matt Audesse <matt@mattaudesse.com>
2019-09-19 11:21:06 -0400

    Fix `geth --dev.period` discrepancy w/ comments + allow envvar override
    Defaults to value of "1" for expedience of local testing/demos, but
    allows toggling for greater correctness' sake (eg in CI).

--- scripts/ethereum-devnet/run.sh
+++ scripts/ethereum-devnet/run.sh
@@ -32,24 +32,28 @@ mkdir -p $LOGDIR
 
 V1_9_X_ARGS=$(geth version 2>/dev/null |grep -q "^Version: 1.9" && echo "--allow-insecure-unlock" || echo "")
 
-# Note: `--dev.period 12` is used to prevent the `geth` miner pausing in the
-# absence of pending transactions to process (e.g. in the case of demo/test
-# suite timeouts) and "12" has been chosen specifically to simulate the nominal
-# block generation speed design goal one should expect to see on the main net:
+# Note: `--dev.period ${GETH_DEV_PERIOD:-1}` is used to prevent the `geth`
+# miner pausing in the absence of pending transactions to process (e.g. in the
+# case of demo/test suite timeouts).
+#
+# Using the nominal block generation speed design goal one should expect to see
+# on the main net is painfully slow for demos/testing, so here we default to "1",
+# but CI should invoke this script with "12" for correctness' sake.
+#
 # https://blog.ethereum.org/2014/07/11/toward-a-12-second-block-time/
 
-# TODO provide option to easily run w/ `--dev.period 1` for quicker demos and
-# to demonstrate that timing is truly pinned to block generation (vs. clock
-# speed)
 geth ${V1_9_X_ARGS} \
   --dev \
-  --dev.period=1 \
+  --dev.period ${GETH_DEV_PERIOD:-1} \
   --mine \
   --identity "AlacrisEthereumDevNet" \
   --datadir $DATADIR \
   --nodiscover \
   --maxpeers 0 \
-  --rpc --rpcapi "db,eth,net,debug,web3,light,personal,admin" --rpcport $RPCPORT --rpccorsdomain "*" \
+  --rpc \
+  --rpcapi "db,eth,net,debug,web3,light,personal,admin" \
+  --rpcport $RPCPORT \
+  --rpccorsdomain "*" \
   --port $PORT \
   --nousb \
   --networkid 17 \
